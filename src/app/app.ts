8e82d52874f2405348cf3edd5092d7048856517d
heguro <65112898+heguro@users.noreply.github.com>
2024-12-16 18:47:17 +0900

    add issue-20241216-1

---
+++ src/app/app.ts
@@ -0,0 +1,5 @@
+import { usedFunc } from "@/fn/util"
+
+export const app = () => {
+  return usedFunc(1, 2);
+};
---
+++ src/fn/util.ts
@@ -0,0 +1,7 @@
+export const usedFunc = (x: number, y: number) => {
+  return x + y;
+};
+
+export const unusedFunc = (x: number, y: number) => {
+  return x - y;
+};
